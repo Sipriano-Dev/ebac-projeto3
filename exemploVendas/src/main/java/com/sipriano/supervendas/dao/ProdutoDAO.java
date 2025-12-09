d0fbf8288194b451c56a0e44a358e2108ac187fd
Anderson Sipriano <adsspns@gmail.com>
2025-12-09 19:29:02 -0300

    Campos em ClientDAO e ProdutoDAO prontos

--- exemploVendas/src/main/java/com/sipriano/supervendas/dao/ProdutoDAO.java
+++ exemploVendas/src/main/java/com/sipriano/supervendas/dao/ProdutoDAO.java
@@ -23,14 +23,15 @@ public class ProdutoDAO extends GenericDAO<Produto, String> implements IProdutoD
         entityCadastrado.setDescricao(entity.getDescricao());
         entityCadastrado.setNome(entity.getNome());
         entityCadastrado.setValor(entity.getValor());
+        entityCadastrado.setCategoria(entity.getCategoria());
     }
 
     @Override
     protected String getQueryInsercao() {
         StringBuilder sb = new StringBuilder();
         sb.append("INSERT INTO TB_PRODUTO ");
-        sb.append("(ID, CODIGO, NOME, DESCRICAO, VALOR)");
-        sb.append("VALUES (nextval('sq_produto'),?,?,?,?)");
+        sb.append("(ID, CODIGO, NOME, DESCRICAO, VALOR, CATEGORIA)");
+        sb.append("VALUES (nextval('sq_produto'),?,?,?,?,?)");
         return sb.toString();
     }
 
@@ -40,6 +41,7 @@ public class ProdutoDAO extends GenericDAO<Produto, String> implements IProdutoD
         stmInsert.setString(2, entity.getNome());
         stmInsert.setString(3, entity.getDescricao());
         stmInsert.setBigDecimal(4, entity.getValor());
+        stmInsert.setString(5, entity.getCategoria());
     }
 
     @Override
@@ -61,6 +63,7 @@ public class ProdutoDAO extends GenericDAO<Produto, String> implements IProdutoD
         sb.append("DESCRICAO = ?,");
         sb.append("VALOR = ?");
         sb.append(" WHERE CODIGO = ?");
+        sb.append(" WHERE CATEGORIA = ?");
         return sb.toString();
     }
 
@@ -71,6 +74,7 @@ public class ProdutoDAO extends GenericDAO<Produto, String> implements IProdutoD
         stmUpdate.setString(3, entity.getDescricao());
         stmUpdate.setBigDecimal(4, entity.getValor());
         stmUpdate.setString(5, entity.getCodigo());
+        stmUpdate.setString(6, entity.getCategoria());
     }
 
     @Override
